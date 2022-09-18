fc2715d78b88a25f52bc5d07f41cd2cfe15486f7
willtucker42 <willtucker42@gmail.com>
2022-09-17 17:06:03 -0700

    Bet buttons started

--- app/src/main/java/com/example/bearvbull/MainActivity.kt
+++ app/src/main/java/com/example/bearvbull/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.bearvbull
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bearvbull.ui.theme.BearVBullTheme
+import com.example.bearvbull.ui.theme.BetGreen
+import com.example.bearvbull.ui.theme.BetRed
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +33,28 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.Black
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Row {
+                            BetButton(
+                                percentage = 54,
+                                icon = R.drawable.arrow_down,
+                                backgroundColor = BetRed,
+                                description = "Down arrow"
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            BetButton(
+                                percentage = 46,
+                                icon = R.drawable.arrow_up,
+                                backgroundColor = BetGreen,
+                                description = "Up arrow"
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -31,7 +63,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", color = Color.White)
+}
+
+@Composable
+fun BetButton(percentage: Int, icon: Int, backgroundColor: Color, description: String) {
+    Column(
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(8.dp))
+            .background(color = backgroundColor),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = description,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            fontStyle = FontStyle.Italic,
+            text = "$percentage%",
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/bearvbull/util/strings.kt
@@ -0,0 +1,3 @@
+package com.example.bearvbull.util
+
+val PERCENT_SIGN = "%"
