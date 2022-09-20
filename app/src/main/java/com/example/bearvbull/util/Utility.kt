306a94fef2692b06f87a5f5b1ace704f0d1e71be
willtucker42 <willtucker42@gmail.com>
2022-09-20 13:56:12 -0700

    Remove space from getFormattedNumber return string

--- app/src/main/java/com/example/bearvbull/util/Utility.kt
+++ app/src/main/java/com/example/bearvbull/util/Utility.kt
@@ -48,7 +48,7 @@ val betInfoTypeList = listOf(
 fun Double.getFormattedNumber(): String {
     if (this < 1000) return "" + this
     val exp = (ln(this) / ln(1000.0)).toInt()
-    return String.format("%.1f %c", this / 1000.0.pow(exp.toDouble()), "kMGTPE"[exp - 1])
+    return String.format("%.1f%c", this / 1000.0.pow(exp.toDouble()), "kMGTPE"[exp - 1])
 }
 
 fun Double.formatBigNumberWithCommas(): String {
