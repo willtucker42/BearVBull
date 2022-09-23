d8269936a4492b53852dc525259803c12cbb548d
willtucker42 <willtucker42@gmail.com>
2022-09-22 20:16:54 -0700

    added profile pics

--- app/src/main/java/com/example/bearvbull/ui/components/RankingsComponents.kt
+++ app/src/main/java/com/example/bearvbull/ui/components/RankingsComponents.kt
@@ -2,11 +2,14 @@ package com.example.bearvbull.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import com.example.bearvbull.util.PodiumRanks
 
 
 @Composable
-fun RankingsPodiumBox() {
+fun RankingsPodiumBox(rank: PodiumRanks) {
     Box() {
-
+        Image(painter = painterResource(id = ))
     }
 }
