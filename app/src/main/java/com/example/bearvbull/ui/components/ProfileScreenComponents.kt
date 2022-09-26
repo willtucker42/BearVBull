0e1f70b011b363a1677c7db8a51291251e6600ab
willtucker42 <willtucker42@gmail.com>
2022-09-25 21:07:55 -0700

    Created TopBar for profile screen

--- app/src/main/java/com/example/bearvbull/ui/components/ProfileScreenComponents.kt
+++ app/src/main/java/com/example/bearvbull/ui/components/ProfileScreenComponents.kt
@@ -1,2 +1,22 @@
 package com.example.bearvbull.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.bearvbull.R
+
+@Composable
+fun ProfileTopBar() {
+    Row(Modifier.height(12.dp)) {
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.settings_gear_icon),
+            contentDescription = "Settings"
+        )
+    }
+}
--- app/src/main/java/com/example/bearvbull/ui/views/ProfileScreen.kt
+++ app/src/main/java/com/example/bearvbull/ui/views/ProfileScreen.kt
@@ -1,9 +1,20 @@
 package com.example.bearvbull.ui.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.bearvbull.ui.theme.DeepPurple
 
 @Composable
 fun ProfileScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DeepPurple),
+    ) {
 
+    }
 }
 
