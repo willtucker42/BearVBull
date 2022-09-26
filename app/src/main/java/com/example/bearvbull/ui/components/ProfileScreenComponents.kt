dbf859b14ca6332f5c42cbe002d499e83634d866
willtucker42 <willtucker42@gmail.com>
2022-09-25 21:01:30 -0700

    Changed Rankings.kt to RankingsScreen.kt, added ProfileScreen.kt and ProfileScreenComponents.kt files

---
+++ app/src/main/java/com/example/bearvbull/ui/components/ProfileScreenComponents.kt
@@ -0,0 +1,2 @@
+package com.example.bearvbull.ui.components
+
---
+++ app/src/main/java/com/example/bearvbull/ui/views/ProfileScreen.kt
@@ -0,0 +1,9 @@
+package com.example.bearvbull.ui.views
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen() {
+
+}
+
