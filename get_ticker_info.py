e968403130b242fdf53f56f37e19e9ca8d74b050
willtucker42 <iamhiding24@gmail.com>
2022-10-15 11:30:44 -0700

    added firebase stuff for python

--- get_ticker_info.py
+++ get_ticker_info.py
@@ -1,4 +1,9 @@
 import yfinance as yf
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('./bearvbull_service_account_key.json')
+default_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 def getTodaysOpen(ticker):
 	ticker_info = yf.Ticker(ticker).info
@@ -9,10 +14,13 @@ def getTodaysOpen(ticker):
 	
 	if (todaysMarketOpen >= previousMarketClose):
 		print('The market opened green today.')
+		# updateBetsAndUsers('green')
 	elif (todaysMarketOpen < previousMarketClose):
 		print('The market opened red today.')
+		# updateBetsAndUsers('red')
 	else:
 		print('error1')
+
 	print('todays market open: ' + str(todaysMarketOpen))
 	print('previous market open: ' + str(previousMarketClose))
 
