51dc6b4b7cce2dc596e06d3be835d26a0bf6e42c
willtucker42 <willtucker42@gmail.com>
2022-10-17 19:18:58 -0700

    createNewMarket function successfully adds a market to firebase db

--- FirebasePythonFunctions/create_new_market.py
+++ FirebasePythonFunctions/create_new_market.py
@@ -19,7 +19,18 @@ def createDocumentNameForMarket(tkr):
 
 
 def createNewMarket(ticker):
-    doc_ref = db.collection(u'live_prediction_market_info').document(u'')
+    doc_ref = db.collection(u'live_prediction_market_info').document(createDocumentNameForMarket(ticker))
+    doc_ref.set({
+        u'bear_headcount': 0,
+        u'bear_total': 0,
+        u'bet_status': u'live',
+        u'biggest_bear_bet': 0,
+        u'biggest_bull_bet': 0,
+        u'bull_headcount': 0,
+        u'bull_total': 0,
+        u'end_time': datetime.datetime.now(),
+        u'ticker': ticker
+    })
 
 
-createDocumentNameForMarket("SPY")
+createNewMarket("SPY")
