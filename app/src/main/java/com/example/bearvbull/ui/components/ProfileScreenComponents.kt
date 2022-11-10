e74d2985e1644b9383321c88b91c048c13100a32
willtucker42 <willtucker42@gmail.com>
2022-11-09 21:05:49 -0800

    trying the new decorationBox for the basictextfield

--- app/src/main/java/com/example/bearvbull/ui/components/ProfileScreenComponents.kt
+++ app/src/main/java/com/example/bearvbull/ui/components/ProfileScreenComponents.kt
@@ -186,7 +186,16 @@ fun UserNameWithEditButton(
                     userNameText = it
                 },
                 modifier = Modifier.width(IntrinsicSize.Min),
-                textStyle = LocalTextStyle.current.copy(color = Color.White)
+                textStyle = LocalTextStyle.current.copy(color = Color.White),
+                decorationBox = {
+                    Box(
+                        modifier =
+                        Modifier.background(Color.LightGray, RoundedCornerShape(percent = 30))
+                            .padding(16.dp)
+                    ) {
+
+                    }
+                }
             )
         } else {
             Text(
