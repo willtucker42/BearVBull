9793d2aa40a709da80e3a304bed66ac6ee2b583e
willtucker42 <willtucker42@gmail.com>
2022-12-12 21:33:26 -0800

    start notifications

--- app/src/main/java/com/example/bearvbull/MainActivity.kt
+++ app/src/main/java/com/example/bearvbull/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.bearvbull
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -34,6 +38,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bearvbull.data.ActiveMarket
 import com.example.bearvbull.data.BetInformation
@@ -55,6 +60,7 @@ import java.util.*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        askNotificationPermission()
         fun googleLogin(): GoogleSignInClient {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -110,7 +116,40 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    // Declare the launcher at the top of your Activity/Fragment:
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                println("permission for notifications granted")
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+                println("The fuck even is this")
+            } else {
+                // Directly ask for the permission
+                println("Directly asking")
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS)
+            }
+        }
+    }
 }
+
 
 @Composable
 fun BottomNavBar(modifier: Modifier, viewModel: MainViewModel, selectedScreen: NavBarItems) {
